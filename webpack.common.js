b09a165f37722ae5dba3e543d31af9c2b4dcd49e
nrtmovrusm <192477100+nrtmovrusm@users.noreply.github.com>
2025-02-17 14:01:56 -0800

    Added ESLint, Prettier, and eslint-config-prettier Added husky and lint-staged for pre-commit hook

--- webpack.common.js
+++ webpack.common.js
@@ -2,34 +2,34 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        app: './src/index.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Production',
-            template: "./src/template.html",
-        }),
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Production",
+      template: "./src/template.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      //   {
+      //     test: /\.html$/i,
+      //     loader: "html-loader",
+      //   },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-        //   {
-        //     test: /\.html$/i,
-        //     loader: "html-loader",
-        //   },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: "asset/resource",
-          },
-        ],
-    },
+  },
 };
